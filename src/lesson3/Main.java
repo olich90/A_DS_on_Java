26e62438014dab07c039cff2b2fd3fea713b990e
olich90 <olich_90@mail.ru>
2021-09-18 21:03:46 +0300

    hw3

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -14,31 +14,72 @@ public class Main {
 //            System.out.println(myStack.pop());
 //        }
 
+        // 2. Создать программу, которая переворачивает вводимые строки (читает справа налево).
+        MyStack<String> myStack = new MyStack<>();
+        myStack.push("т");
+        myStack.push("e");
+        myStack.push("в");
+        myStack.push("и");
+        myStack.push("р");
+        myStack.push("П");
+
+        for (int i = 0; i < 6; i++) {
+            System.out.print(myStack.pop());
+        }
+
 //        Expression e = new Expression("()+{}+[{}]");
 //        System.out.println(e.checkBracket());
 
 
-//        MyQueue<Integer> queue = new MyQueue<>();
+//        MyQueue<Integer> queue = new MyQueue<>(5);
 //
 //        queue.insert(3);
 //        queue.insert(2);
 //        queue.insert(7);
 //        queue.insert(4);
+//        queue.insert(5);
+//        queue.insert(6);
 //
-//        for (int i = 0; i < 4; i++) {
+//        for (int i = 0; i < 6; i++) {
 //            System.out.println(queue.remove());
 //        }
 
-        MyPriorityQueue<Integer> mpq = new MyPriorityQueue<>();
+//        MyPriorityQueue<Integer> mpq = new MyPriorityQueue<>();
+//
+//        mpq.insert(5);
+//        mpq.insert(2);
+//        mpq.insert(20);
+//        mpq.insert(21);
+//        mpq.insert(4);
+//
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println(mpq.remove());
+//        }
 
-        mpq.insert(5);
-        mpq.insert(2);
-        mpq.insert(20);
-        mpq.insert(21);
-        mpq.insert(4);
+//        MyDeque<Integer> deque = new MyDeque<>(5);
+//
+//        deque.insertLeft(2);
+//        deque.insertLeft(6);
+//        deque.insertLeft(4);
+//        deque.insertLeft(7);
+//
+//
+//        for (int i = 0; i < 2; i++) {
+//            System.out.print(deque.removeLeft() + " ");
+//        }
+//
+//        deque.insertRight(8);
+//        System.out.println();
+//        System.out.println(deque.peekRight());
+//
+//        deque.insertRight(1);
+//        System.out.println(deque.peekRight());
+//
+//        deque.insertRight(2);
+//        System.out.println(deque.peekRight());
+//
+//        deque.insertLeft(99);
+//        System.out.println(deque.peekLeft());
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println(mpq.remove());
-        }
     }
 }
