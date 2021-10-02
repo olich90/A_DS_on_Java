95a962268c6f1014e11a4b392c505be6262d856b
olich90 <olich_90@mail.ru>
2021-10-02 20:30:45 +0300

    l7

--- src/lesson7/Main.java
+++ src/lesson7/Main.java
@@ -2,13 +2,19 @@ package lesson7;
 
 public class Main {
     public static void main(String[] args) {
-        Graph graph = new Graph(5);
+        Graph graph = new Graph(10);
 
         graph.addEdge(1, 2);
         graph.addEdge(0, 4);
         graph.addEdge(1, 4);
         graph.addEdge(1, 3);
-        graph.addEdge(1, 0);
+        graph.addEdge(4, 5);
+        graph.addEdge(4, 6);
+        graph.addEdge(6, 9);
+        graph.addEdge(8, 5);
+        graph.addEdge(7, 9);
+        graph.addEdge(9, 8);
+        graph.addEdge(5, 7);
 
 //        System.out.println(graph.getVertexCount());
 //        System.out.println(graph.getEdgeCount());
@@ -18,9 +24,9 @@ public class Main {
 //        System.out.println(dfp.hasPathTo(0));
 //        System.out.println(dfp.pathTo(0));
 
-        BreadthFirstPath bfp = new BreadthFirstPath(graph, 2);
-        System.out.println(bfp.hasPathTo(0));
-        System.out.println(bfp.pathTo(0));
+        BreadthFirstPath bfp = new BreadthFirstPath(graph, 0);
+        System.out.println(bfp.hasPathTo(5));
+        System.out.println(bfp.pathTo(9));
 
     }
 }
